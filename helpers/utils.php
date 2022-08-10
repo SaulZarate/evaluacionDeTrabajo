4f2e4ba35f4000d772f0fa5a81e38691c518a14a
Saul Zarate <saul.zarate@davinci.edu.ar>
2022-08-09 22:51:21 -0300

    Alert arreglado

--- helpers/utils.php
+++ helpers/utils.php
@@ -21,7 +21,7 @@ function showAlertOk($message, $time=3000){
             </symbol>
         </svg>
 
-        <div class='alert alert-success d-flex align-items-center' role='alert' id={$idElement}>
+        <div class='alert alert-success d-flex align-items-center alertCustom container' role='alert' id={$idElement}>
             <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success:'>
                 <use xlink:href='#check-circle-fill' />
             </svg>
@@ -55,7 +55,7 @@ function showAlertError($message, $time=3000){
             </symbol>
         </svg>
 
-        <div class='alert alert-danger d-flex align-items-center' role='alert' id={$idElement}>
+        <div class='alert alert-danger d-flex align-items-center alertCustom container' role='alert' id={$idElement}>
             <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
             <div>
             {$message}
